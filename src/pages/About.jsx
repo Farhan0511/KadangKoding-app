079b0c4ecec91226a25a1f81d0300ccff62d594d
farhan0511 <programingsukses@gmail.com>
2026-08-15 10:05:36 +0700

    feat: add footer component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function About() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex flex-col items-center justify-center p-24">
+      <main className="flex flex-1 flex-col items-center justify-center p-24">
         <h1 className="text-3xl font-bold underline">About Us</h1>
         <p className="mt-4 text-lg text-center">
           Welcome to KadangKoding! We are passionate about providing
@@ -12,7 +13,8 @@ export default function About() {
           levels improve their skills. Our mission is to make coding accessible
           and enjoyable for everyone.
         </p>
-      </div>
+      </main>
+      <Footer />
     </div>
   );
 }
