7fe1c6193c4e62b30e660b484b9ba413ca90e288
farhan0511 <programingsukses@gmail.com>
2026-08-16 22:24:33 +0700

    feat: add favorite components

---
+++ src/components/Favorite.jsx
@@ -0,0 +1,15 @@
+export default function OurFavorite() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-normal sm:text-3xl md:text-5xl leading-tight">
+        Our Favourite
+        <br />
+        Collection
+      </h1>
+      <p className="mt-4 text-lg md:text-xl text-gray-500 max-w-xl">
+        We are inspired by the realities of life today, in which traditional
+        divides between personal and professional space are more fluid.
+      </p>
+    </div>
+  );
+}
