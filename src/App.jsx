cb40074a0d2f8ba3df6de6341f797452fc4d5afe
farhan0511 <programingsukses@gmail.com>
2026-08-19 10:00:44 +0700

    fix: landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Favorite from "./components/atoms/Favorite";
+import Quotes from "./components/atoms/Quotes";
+import Subscribe from "./components/atoms/Subscribe";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Hero />
+      <main className="flex-1">
+        <Hero />
+
+        <Favorite />
+
+        <Quotes />
+
+        <Subscribe />
+      </main>
+      <Footer />
     </div>
   );
 }
-  
